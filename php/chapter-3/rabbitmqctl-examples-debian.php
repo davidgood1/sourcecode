6220cc37208023acaa6cbc134c2b5253a077c4eb
David Good <david.good1@gmail.com>
2016-01-02 20:02:35 -0600

    Added Chapter 3 php example that works with debian packages

---
+++ php/chapter-3/rabbitmqctl-examples-debian.php
@@ -0,0 +1,32 @@
+<?php
+
+// This version works on Debian when using the php-amqplib Debian package
+
+spl_autoload_register(
+    function ($class) {
+        include str_replace("\\", "/", $class) . '.php';
+    }
+);
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+define('HOST', 'localhost');
+define('PORT', 5672);
+define('USER', 'guest');
+define('PASS', 'guest');
+define('VHOST', '/');
+
+$conn = new AMQPStreamConnection(HOST, PORT, USER, PASS, VHOST);
+//$conn = new AMQPConnection(HOST, PORT, USER, PASS);
+$channel = $conn->channel();
+
+$channel->exchange_declare('logs-exchange', 'topic', false, true, false);
+
+$channel->queue_declare('msg-inbox-errors', false, true, false, false);
+$channel->queue_declare('msg-inbox-logs', false, true, false, false);
+$channel->queue_declare('all-logs', false, true, false, false);
+
+$channel->queue_bind('msg-inbox-errors', 'logs-exchange', 'error.msg-inbox');
+$channel->queue_bind('msg-inbox-logs', 'logs-exchange', '*.msg-inbox');
+
+?>
